673c19bedef19f0737dc84975eb92541676c80f3
XC-Work <455908283@qq.com>
2023-02-01 14:51:06 +0800

    创建controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  def create
+    user = User.new email:'455@qq.com', name: 'xc'
+    if user.save
+      render json: user
+    else
+      render json: user.errors
+    end
+  end
+
+  def show
+    user = User.find_by_id params[:id]
+    if user
+      render json: user
+    else
+      head 404
+    end
+  end
+end
