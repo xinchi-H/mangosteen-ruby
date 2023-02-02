1c67efaa36ad097c2264a8efbf68851798db06dc
XC-Work <455908283@qq.com>
2023-02-02 18:18:40 +0800

    创建validation_codes

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_11_085943) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_02_095405) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "validation_codes", force: :cascade do |t|
+    t.string "email"
+    t.integer "kind", default: 1, null: false
+    t.datetime "used_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
