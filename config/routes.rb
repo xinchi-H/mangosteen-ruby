6ff822a70fd7bf2d317c40cd5ca4630b3357f8ac
XC-Work <455908283@qq.com>
2023-02-02 17:13:32 +0800

    API

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  namespace :api do
+    namespace :v1 do
+      resources :validation_codes, only: [:create]
+      resource :session, only: [:create, :destroy]
+      resource :me, only: [:show]
+      resources :item
+      resources :tags
+    end
+  end
 
 end
