44953957a8a53009e4aa3c38a0af6fcd0f0af66c
XC-Work <455908283@qq.com>
2023-03-10 17:32:23 +0800

    create_validation_codes的验证码

--- db/migrate/20230202095405_create_validation_codes.rb
+++ db/migrate/20230202095405_create_validation_codes.rb
@@ -3,6 +3,7 @@ class CreateValidationCodes < ActiveRecord::Migration[7.0]
     create_table :validation_codes do |t|
       t.string :email
       t.integer :kind, default: 1, null: false
+      t.string :code, limit: 50
       t.datetime :used_at
 
       t.timestamps
