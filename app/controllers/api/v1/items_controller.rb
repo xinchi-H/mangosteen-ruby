6c2cc6b4ea811216896d4ab61f26ad16356a7dc7
XC-Work <455908283@qq.com>
2023-03-20 16:08:47 +0800

    实现分页

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,5 +1,11 @@
 class Api::V1::ItemsController < ApplicationController
   def index
+    items = Item.page params[:page].per(10)
+    render json: { resources: items, pager: {
+      page: params[:page],
+      per_page: 10,
+      count: Item.count,
+    }}
   end
   def create
     item = Item.new amount: 1
