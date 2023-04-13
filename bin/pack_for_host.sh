6a1e04246cac1973827927149098b0c7c3d32f60
XC-Work <455908283@qq.com>
2023-04-13 16:26:15 +0800

    在宿主机里部署应用的脚本

---
+++ bin/pack_for_host.sh
@@ -0,0 +1,17 @@
+dir=oh-my-env
+
+time=$(date +'%Y%m%d-%H%M%S')
+dist=tmp/mangosteen-$time.tar.gz
+current_dir=$(dirname $0)
+deploy_dir=/workspaces/$dir/mangosteen_deploy
+
+yes | rm tmp/mangosteen-*.tar.gz; 
+yes | rm $deploy_dir/mangosteen-*.tar.gz; 
+
+tar --exclude="tmp/cache/*" -czv -f $dist *
+mkdir -p $deploy_dir
+cp $current_dir/../config/host.Dockerfile $deploy_dir/Dockerfile
+cp $current_dir/setup_host.sh $deploy_dir/
+mv $dist $deploy_dir
+echo $time > $deploy_dir/version
+echo 'DONE!'
